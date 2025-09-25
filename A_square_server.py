df1df610025ec65e4dbc3169a40eab7ebb82b259
mano28dev <mano.rp.dev@gmail.com>
2025-09-25 12:22:18 +0530

    UI final v1

--- A_square_server.py
+++ A_square_server.py
@@ -10,4 +10,9 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route("/upload")
+def upload():
+    pass
+    # return render_template("about.html")
+
 app.run(host="0.0.0.0", port=5000)
